8f2f045a4ba9328021252b851bfe6c105de661ea
danie <danie@Diaz>
2023-10-12 20:19:22 -0500

    (y)

--- Movimientos Caballo/src/Caballo/Caballo.java
+++ Movimientos Caballo/src/Caballo/Caballo.java
@@ -1,55 +1,69 @@
 package Caballo;
 
 public class Caballo {
-	
-	private int fila, columna;
-	private int posicion[][];
-	private int x[]= {columna+2, columna+1, columna-2, columna-1};
-	private int y[]= {fila+2, fila+1, fila-2, fila-1};
-	
-	public Caballo(int[][] posicion) {
-		this.x= new int[8];
-		this.y= new int [8];
-		this.posicion= posicion;
-		posicion= new int[8][8];
-		
-		for(int i=0; i<8; i++) {
-			for(int j=0; j<8; j++) {
-				posicion[i][j]=0;
-			}
-		}
-		
-		/* 1) No se debe desbordar en movimientos hacia arriba o hacia abajo
-		 * 2) Debe verificar que no haya un 1 en la posicion deseada para moverse
-		 * 3) PENE
-		 * 4) Chupan
-		 * 5) szs 
-		 * 6) rico 
-		 */
-		
-	}
-	
-	private boolean resolver(int fila, int columna) throws EImposible{
-		if(fila<0 || fila>8 || columna<0 || columna>0) {
-			throw new EImposible("La ficha no se encuentra en el tablero");
-		}else if(posicion[fila][columna]==1){
-				throw new EImposible("Ya hizo este movimiento");
-			}else {
-				posicion[fila][columna]=1;
-				
-			
-		}
-		
-	}
-	
-	public boolean[] movimientos(int fila, int columna) throws EImposible{
-		for(int i=0; i<4; i++) {
-			for(int j=0; j<4; j++) {
-				if(y[j]<0 || y[i]>8) {
-					throw new EImposible("Movimiento no valido");
-				}
-			}
-		}
-	}
+    private int fila, columna;
+    private int[][] posicion;
 
+    public Caballo(int[][] posicion) {
+        this.posicion = posicion;
+        this.fila = 0;
+        this.columna = 0;
+        this.posicion = new int[8][8];
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.posicion[i][j] = 0;
+            }
+        }
+    }
+
+    public boolean resolver(int fila, int columna) throws EImposible {
+        if (fila < 0 || fila >= 8 || columna < 0 || columna >= 8) {
+            throw new EImposible("La ficha no se encuentra en el tablero");
+        } else if (posicion[fila][columna] == 1) {
+            throw new EImposible("Ya hizo este movimiento");
+        } else {
+            posicion[fila][columna] = 1;
+            return true;
+        }
+    }
+
+    public boolean[] movimientos() throws EImposible {
+        boolean[] movimientosValidos = new boolean[8];
+        
+        //estos son los desplazamientos posibles
+
+        int[] dx = { -2, -1, 1, 2, -2, -1, 1, 2 };
+        int[] dy = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+        for (int i = 0; i < 8; i++) {
+            int nuevaFila = fila + dx[i];
+            int nuevaColumna = columna + dy[i];
+
+            if (nuevaFila >= 0 && nuevaFila < 8 && nuevaColumna >= 0 && nuevaColumna < 8) {
+                if (posicion[nuevaFila][nuevaColumna] == 0) {
+                    movimientosValidos[i] = true;
+                } else {
+                    movimientosValidos[i] = false;
+                }
+            } else {
+                movimientosValidos[i] = false;
+            }
+        }
+
+        if (noHayMovimientosValidos(movimientosValidos)) {
+            throw new EImposible("No hay movimientos válidos desde la posición actual.");
+        }
+
+        return movimientosValidos;
+    }
+
+    private boolean noHayMovimientosValidos(boolean[] movimientosValidos) {
+        for (boolean valido : movimientosValidos) {
+            if (valido) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
