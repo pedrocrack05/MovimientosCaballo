35c36c79220d3418f7ad4eadaf791616a3b7b658
TH_PE <TH_PE@192.168.1.93>
2023-10-22 00:08:24 -0500

    El "verdadero"

--- Movimientos Caballo/src/Caballo/Caballo.java
+++ Movimientos Caballo/src/Caballo/Caballo.java
@@ -9,13 +9,13 @@ public class Caballo {
 	private static int[] dy = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
 	public Caballo(int fila, int columna) {
-	    tablero = new int[N][N];
-	    try {
-	    resolver(fila, columna, 1);
-	    }catch(EImposible e){
-	    	System.out.println(e.getMessage());
-	    }
-	    imprimirTablero();
+		tablero = new int[N][N];
+		try {
+			resolver(fila, columna, 1);
+		}catch(EImposible e){
+			System.out.println(e.getMessage());
+		}
+		imprimirTablero();
 	}
 
 	public void resolver(int fila, int columna, int intento) throws EImposible {
@@ -23,7 +23,7 @@ public class Caballo {
 			imprimirTablero();
 			return;
 		}
-		
+
 		if (fila < 0 || fila >= N || columna < 0 || columna >= N) {
 			throw new EImposible("El caballo no se encuentra en el tablero");
 		} else if (tablero[fila][columna] != 0) {
@@ -38,7 +38,7 @@ public class Caballo {
 					k++;
 				}
 			}
-			
+
 			int movimientosSolucion[]= new int[k];
 			int j=0;
 			for(int i=0; i<N;i++) {
@@ -47,43 +47,44 @@ public class Caballo {
 					j++;
 				}
 			}
-            System.out.println(intento+"= "+fila+", "+columna);
-			
-            int solucion= warnsdorff(fila, columna, movimientosSolucion);
-            if(solucion==-1) {
-            	tablero[fila][columna]=0;
-            	return;
-            }else {
-            
-            resolver(fila+dy[movimientosSolucion[solucion]], columna+dx[movimientosSolucion[solucion]], intento+1);
-            tablero[fila][columna]=0;
-           }
+			System.out.println(intento+"= "+fila+", "+columna);
+
+			int solucion= warnsdorff(fila, columna, movimientosSolucion);
+			if(solucion==-1) {
+				tablero[fila][columna]=0;
+				return;
+			}else {
+
+				resolver(fila+dy[movimientosSolucion[solucion]], columna+dx[movimientosSolucion[solucion]], intento+1);
+				tablero[fila][columna]=0;
+			}
 
 		}
 	}
-	
-	public static int warnsdorff(int fila, int columna, int[] movimientosSolucion)throws EImposible {
-		
-		int[] futuros = new int[movimientosSolucion.length];
-		for(int i=0; i<movimientosSolucion.length; i++) {
-			boolean [] movimientosSiguienteCasilla= movimientos(fila+dy[movimientosSolucion[i]], columna+dx[movimientosSolucion[i]]);
-				if(movimientosSiguienteCasilla[i]==true) {
-					futuros[i]+=1;
-				}
-			}
-				
-		int mejorSolucion[]= futuros;
-		int indice=0;
-		Arrays.sort(mejorSolucion);
-		for(int i=0; i<futuros.length; i++) {
-			if(futuros[i]== mejorSolucion[0]) {
-				indice= i;
-				break;
-			}
-		}
-		return indice;
-		
-	}	
+
+	public static int warnsdorff(int fila, int columna, int[] movimientosSolucion) throws EImposible {
+	    int[] futuros = new int[movimientosSolucion.length];
+	    for (int i = 0; i < movimientosSolucion.length; i++) {
+	        boolean[] movimientosSiguienteCasilla = movimientos(fila + dy[movimientosSolucion[i]], columna + dx[movimientosSolucion[i]]);
+	        for (int j = 0; j < 8; j++) {
+	            if (movimientosSiguienteCasilla[j]) {
+	                futuros[i] += 1;
+	            }
+	        }
+	    }
+
+	    int mejorMovimiento = -1;
+	    int mejorValor = Integer.MAX_VALUE;  // Utilizar Integer.MAX_VALUE
+
+	    for (int i = 0; i < futuros.length; i++) {
+	        if (futuros[i] < mejorValor) {
+	            mejorValor = futuros[i];
+	            mejorMovimiento = i;
+	        }
+	    }
+
+	    return mejorMovimiento;
+	}
 
 	public static boolean[] movimientos(int fila, int columna) throws EImposible {
 		boolean[] movimientosValidos = new boolean[N];
@@ -118,10 +119,10 @@ public class Caballo {
 
 		return false;
 	}
-	
-	 private boolean esMovimientoValido(int x, int y) {
-	        return (x >= 0 && x < N && y >= 0 && y < N && tablero[x][y] == 0);
-	    }
+
+	private boolean esMovimientoValido(int x, int y) {
+		return (x >= 0 && x < N && y >= 0 && y < N && tablero[x][y] == 0);
+	}
 
 	public void imprimirTablero() {
 		for (int i = 0; i < 8; i++) {
@@ -135,6 +136,6 @@ public class Caballo {
 
 	public static void main(String[] args) {
 
-		Caballo caballo= new Caballo(0, 0);
+		Caballo caballo= new Caballo(7, 3);
 	}
 }
