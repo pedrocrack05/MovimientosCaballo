a3c9ad142e73159e7b56398c73f7f79f6623d87f
TH_PE <TH_PE@192.168.1.93>
2023-10-21 22:46:39 -0500

    El mejor hasta el momento

--- Movimientos Caballo/src/Caballo/Caballo.java
+++ Movimientos Caballo/src/Caballo/Caballo.java
@@ -3,31 +3,37 @@ package Caballo;
 import java.util.Arrays;
 
 public class Caballo {
+	private static int N=8;
 	private static int[][] tablero;
 	private static int[] dx = { -2, -1, 1, 2, -2, -1, 1, 2 };
 	private static int[] dy = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
 	public Caballo(int fila, int columna) {
-	    tablero = new int[8][8];
-
-	    for (int i = 0; i < 8; i++) {
-	        for (int j = 0; j < 8; j++) {
-	            tablero[i][j] = 0;
-	        }
-	    }   
+	    tablero = new int[N][N];
+	    try {
+	    resolver(fila, columna, 1);
+	    }catch(EImposible e){
+	    	System.out.println(e.getMessage());
+	    }
+	    imprimirTablero();
 	}
 
-	public int resolver(int fila, int columna, int intento) throws EImposible {
-		if (fila < 0 || fila >= 8 || columna < 0 || columna >= 8) {
-			return -1;
-		} else if (tablero[fila][columna] == 1) {
-			return -1;
+	public void resolver(int fila, int columna, int intento) throws EImposible {
+		if(intento > N*N) {
+			imprimirTablero();
+			return;
+		}
+		
+		if (fila < 0 || fila >= N || columna < 0 || columna >= N) {
+			throw new EImposible("El caballo no se encuentra en el tablero");
+		} else if (tablero[fila][columna] != 0) {
+			throw new EImposible("El caballo ya estuvo aqui");
 		} else {
-			tablero[fila][columna] = 1;
+			tablero[fila][columna] = intento;
 
 			boolean movimientos[]= movimientos(fila, columna);
 			int k = 0;
-			for (int i = 0; i<8; i++) {
+			for (int i = 0; i<N; i++) {
 				if (movimientos[i] == true) {
 					k++;
 				}
@@ -35,7 +41,7 @@ public class Caballo {
 			
 			int movimientosSolucion[]= new int[k];
 			int j=0;
-			for(int i=0; i<8;i++) {
+			for(int i=0; i<N;i++) {
 				if(movimientos[i]== true) {
 					movimientosSolucion[j]= i;
 					j++;
@@ -44,47 +50,52 @@ public class Caballo {
             System.out.println(intento+"= "+fila+", "+columna);
 			
             int solucion= warnsdorff(fila, columna, movimientosSolucion);
-            return resolver(fila+dx[solucion], columna+dy[solucion], intento+1);
+            if(solucion==-1) {
+            	tablero[fila][columna]=0;
+            	return;
+            }else {
+            
+            resolver(fila+dy[movimientosSolucion[solucion]], columna+dx[movimientosSolucion[solucion]], intento+1);
+            tablero[fila][columna]=0;
+           }
 
 		}
 	}
 	
 	public static int warnsdorff(int fila, int columna, int[] movimientosSolucion)throws EImposible {
-		int[] cantidadSiguientesCasillas = new int[movimientosSolucion.length + 1];
+		
+		int[] futuros = new int[movimientosSolucion.length];
 		for(int i=0; i<movimientosSolucion.length; i++) {
-			boolean [] movimientosSiguienteCasilla= movimientos(fila+dx[movimientosSolucion[i]], columna+dy[movimientosSolucion[i]]);
-			for(int j=0; j<8; j++) {
-				if(movimientosSiguienteCasilla[j]==true) {
-					cantidadSiguientesCasillas[i]+=1;
+			boolean [] movimientosSiguienteCasilla= movimientos(fila+dy[movimientosSolucion[i]], columna+dx[movimientosSolucion[i]]);
+				if(movimientosSiguienteCasilla[i]==true) {
+					futuros[i]+=1;
 				}
 			}
-		}
 				
-		int mejorSolucion= cantidadSiguientesCasillas[0];
-		for(int i=1; i<cantidadSiguientesCasillas.length; i++) {
-			if(mejorSolucion>cantidadSiguientesCasillas[i]) {
-				mejorSolucion= cantidadSiguientesCasillas[i];
+		int mejorSolucion[]= futuros;
+		int indice=0;
+		Arrays.sort(mejorSolucion);
+		for(int i=0; i<futuros.length; i++) {
+			if(futuros[i]== mejorSolucion[0]) {
+				indice= i;
+				break;
 			}
 		}
+		return indice;
 		
-		return mejorSolucion;
 	}	
 
 	public static boolean[] movimientos(int fila, int columna) throws EImposible {
-		boolean[] movimientosValidos = new boolean[8];
+		boolean[] movimientosValidos = new boolean[N];
 
 		//estos son los desplazamientos posibles
 
-		for (int i = 0; i < 8; i++) {
-			int nuevaFila = fila + dx[i];
-			int nuevaColumna = columna + dy[i];
+		for (int i = 0; i < N; i++) {
+			int nuevaFila = fila + dy[i];
+			int nuevaColumna = columna + dx[i];
 
-			if (nuevaFila >= 0 && nuevaFila < 8 && nuevaColumna >= 0 && nuevaColumna < 8) {
-				if (tablero[nuevaFila][nuevaColumna] == 0) {
-					movimientosValidos[i] = true;
-				} else {
-					movimientosValidos[i] = false;
-				}
+			if (nuevaFila >= 0 && nuevaFila < N && nuevaColumna >= 0 && nuevaColumna < N && tablero[nuevaFila][nuevaColumna]==0) {
+				movimientosValidos[i] = true;
 			} else {
 				movimientosValidos[i] = false;
 			}
@@ -107,12 +118,16 @@ public class Caballo {
 
 		return false;
 	}
+	
+	 private boolean esMovimientoValido(int x, int y) {
+	        return (x >= 0 && x < N && y >= 0 && y < N && tablero[x][y] == 0);
+	    }
 
-	public static void imprimirTablero(Caballo caballo) {
+	public void imprimirTablero() {
 		for (int i = 0; i < 8; i++) {
 			System.out.print("[");
 			for (int j = 0; j < 8; j++) {
-				System.out.print(tablero[i][j]+" , ");
+				System.out.print(tablero[i][j]+" ");
 			}
 			System.out.print("]\n");
 		}
@@ -121,12 +136,5 @@ public class Caballo {
 	public static void main(String[] args) {
 
 		Caballo caballo= new Caballo(0, 0);
-		try{
-			System.out.println(caballo.resolver(0, 0, 0));
-		}catch(EImposible e) {
-			System.out.println(e.getMessage());
-		}
-		
-		imprimirTablero(caballo);
 	}
 }
