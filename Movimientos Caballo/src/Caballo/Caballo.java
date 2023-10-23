4cc0d134b3d0dd9b69000601179ca78c4938d46e
BuildTools <unconfigured@null.spigotmc.org>
2023-10-22 19:03:17 -0500

    Full

--- Movimientos Caballo/src/Caballo/Caballo.java
+++ Movimientos Caballo/src/Caballo/Caballo.java
@@ -1,11 +1,14 @@
 package Caballo;
 
+import java.util.Arrays;
+
 public class Caballo {
-	private static int N = 8;
+	private final static int N = 8;
 	private static int[][] tablero;
 	private static int[] dx = { -2, -1, 1, 2, -2, -1, 1, 2 };
 	private static int[] dy = { 1, 2, 2, 1, -1, -2, -2, -1 };
-	private int[][] pasos = new int[64][2];
+	private int[][] pasos = new int[N * N][2];
+	// int contador = 0;
 
 	public Caballo(int fila, int columna) {
 		tablero = new int[N][N];
@@ -15,62 +18,87 @@ public class Caballo {
 			System.out.println(e.getMessage());
 		}
 		imprimirTablero();
-		// System.out.println(intento);
 	}
 
-	public void resolver(int fila, int columna, int intento) throws EImposible {
-		// this.intento++;
+	public void resolver(int fila, int columna, int salto) throws EImposible {
+		// this.contador++;
+
 		if (fila < 0 || fila >= N || columna < 0 || columna >= N) {
 			throw new EImposible("El caballo no se encuentra en el tablero");
 		} else if (tablero[fila][columna] != 0) {
 			throw new EImposible("El caballo ya estuvo aqui");
 		} else {
-			tablero[fila][columna] = intento;
+			tablero[fila][columna] = salto;// Guarda salto en tablero
+			guardarPaso(salto - 1, fila, columna);
+
+			// Guardar los movimientos que puede y no puede hacer desde la pos actual
 			boolean movimientos[] = movimientos(fila, columna);
-			int k = 0;
-			for (int i = 0; i < N; i++) {
-				if (movimientos[i]) {
+
+			// Guardar solamente el índice de los movimientos posibles
+			int[] movimientosPosibles = indicesPosibles(movimientos);
+
+			// System.out.println(salto + "= " + fila + ", " + columna);
+
+			int nextFila, nextColumna;
+			if (salto < (N * N) - 1) {// Si estamos en saltos menores al penúltimo salto
+
+				// Se verifica el camino más corto
+				int mejorSolucion = warnsdorff(fila, columna, movimientosPosibles);
+				// Se actualiza la posición en x e y
+				nextFila = fila + dy[movimientosPosibles[mejorSolucion]];
+				nextColumna = columna + dx[movimientosPosibles[mejorSolucion]];
+				// Llamada recursiva al siguiente movimiento
+				resolver(nextFila, nextColumna, salto + 1);
+			} else {
+				// Se guarda el penúltimo movimiento en la matriz de pasos
+				guardarPaso(salto - 1, fila, columna);
+				// Se actualiza la posición del caballo
+				nextFila = fila + dy[movimientosPosibles[0]];
+				nextColumna = columna + dx[movimientosPosibles[0]];
+				// Se guarda el último movimiento del caballo en la matriz de pasos
+				guardarPaso(salto, nextFila, nextColumna);
+				// Se guarda la posición del caballo en el tablero
+				tablero[nextFila][nextColumna] = salto + 1;
+				return;
+			}
+		}
+	}
+
+	public void guardarPaso(int salto, int fila, int columna) {
+		pasos[salto][0] = fila;
+		pasos[salto][1] = columna;
+	}
+
+	public int[] indicesPosibles(boolean[] movimientos) {
+		// Guarda en un arreglo solamente los pasos que sean posibles
+		int[] posibles = new int[0];
+		int k = 0;
+		for (int i = 0; i < N; i++) {
+			if (movimientos[i]) {
+				if (posibles == null) {
+					posibles = Arrays.copyOf(posibles, 1);
+					posibles[k] = i;
+					k++;
+				} else {
+					posibles = Arrays.copyOf(posibles, posibles.length + 1);
+					posibles[posibles.length - 1] = i;
 					k++;
 				}
 			}
 
-			int movimientosSolucion[] = new int[k];// Se realiza un arreglo con los movimientos que se pueden hacer
-			int j = 0;
-			for (int i = 0; i < N; i++) {
-				if (movimientos[i]) {
-					movimientosSolucion[j] = i;
-					j++;
-				}
-			}
-			System.out.println(intento + "= " + fila + ", " + columna);
-			pasos[intento - 1][0] = fila;
-			pasos[intento - 1][1] = columna;
-
-			if (intento < 63) {
-				int solucion = warnsdorff(fila, columna, movimientosSolucion);
-				resolver(fila + dy[movimientosSolucion[solucion]], columna + dx[movimientosSolucion[solucion]],
-						intento + 1);
-			} else {
-				System.out.println((intento + 1) + "= " + (fila + dy[movimientosSolucion[0]]) + ", "
-						+ (columna + dx[movimientosSolucion[0]]));
-				pasos[intento - 1][0] = fila;
-				pasos[intento - 1][1] = columna;
-				tablero[fila + dy[movimientosSolucion[0]]][columna + dx[movimientosSolucion[0]]] = intento + 1;
-				return;
-			}
-
-			// tablero[fila][columna] = 0;
-
 		}
+
+		return posibles;
 	}
 
-	public static int warnsdorff(int fila, int columna, int[] movimientosSolucion) throws EImposible {
-		int[] futuros = new int[movimientosSolucion.length];
-		for (int i = 0; i < movimientosSolucion.length; i++) {
-			boolean[] movimientosSiguienteCasilla = movimientos(fila + dy[movimientosSolucion[i]],
-					columna + dx[movimientosSolucion[i]]);
+	public static int warnsdorff(int fila, int columna, int[] movimientosPosibles) throws EImposible {
+		int[] futuros = new int[movimientosPosibles.length];
+
+		for (int i = 0; i < movimientosPosibles.length; i++) {
+			boolean[] movimientosSiguiente = movimientos(fila + dy[movimientosPosibles[i]],
+					columna + dx[movimientosPosibles[i]]);
 			for (int j = 0; j < 8; j++) {
-				if (movimientosSiguienteCasilla[j]) {
+				if (movimientosSiguiente[j]) {
 					futuros[i] += 1;
 				}
 			}
@@ -132,6 +160,14 @@ public class Caballo {
 			}
 			System.out.print("]\n");
 		}
+
+		for (int i = 0; i < 64; i++) {
+			System.out.print("[");
+			for (int j = 0; j < 2; j++) {
+				System.out.print(pasos[i][j] + "\t");
+			}
+			System.out.print("]\n");
+		}
 	}
 
 	public int[][] getPasos() {
@@ -140,6 +176,7 @@ public class Caballo {
 
 	public static void main(String[] args) {
 
-		new Caballo(3, 6);
+		Caballo caballo = new Caballo(0, 0);
+		// System.out.println(caballo.contador);
 	}
 }
