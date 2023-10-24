7b3a9c47214f7872d511e947f666ed868b49eedf
german.rivera <german.rivera@RiveraGerman.eia.edu.co>
2023-10-23 20:54:36 -0500

    Added: Paso.java

---
+++ Movimientos Caballo/src/Caballo/Paso.java
@@ -0,0 +1,19 @@
+package Caballo;
+
+public class Paso {
+	private int x, y;
+
+	public Paso(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
